4c105f9ed315b8b41e10e3782f71020fcd2cc5b6
Uillian Gabriel Valerio Da Silva <38649488862@info.local>
2021-02-05 11:11:10 -0300

    Subindo login

---
+++ Componentes/Rodapé/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rodape = () => {
+    return (
+        <footer className="text-center" style={{ marginTop: '70px' }}>
+            <h1>SENAI Informática</h1>
+            <small>Desenvolvido por <strong>Uillian Gabriel</strong></small>
+        </footer>
+    );
+}
+
+export default Rodape;
+
